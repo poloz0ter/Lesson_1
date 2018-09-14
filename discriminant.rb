6cd84596e5b1ad2058431e12c23d650f2d6aca6f
poloz0ter <36717350+poloz0ter@users.noreply.github.com>
2018-09-14 11:33:17 +0400

    Update discriminant.rb

--- discriminant.rb
+++ discriminant.rb
@@ -9,9 +9,9 @@ c = gets.to_f
 
 discr = (b**2) - (4 * a * c)
 puts "Discriminant = #{discr}"
-discr_root = Math.sqrt(discr)
 
 if discr > 0
+  discr_root = Math.sqrt(discr)
   x1 = (-b + discr_root) / (2 * a)
   x2 = (-b - discr_root) / (2 * a)
   puts "x1 = #{x1}, x2 = #{x2}"
