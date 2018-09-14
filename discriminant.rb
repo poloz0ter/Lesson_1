e4247b182ad8e209fea633ba2b0149e3d65fedb8
poloz0ter <36717350+poloz0ter@users.noreply.github.com>
2018-09-14 11:26:26 +0400

    Error correction

--- discriminant.rb
+++ discriminant.rb
@@ -6,16 +6,18 @@ b = gets.to_f
 print "c="
 c = gets.to_f
 
+
 discr = (b**2) - (4 * a * c)
 puts "Discriminant = #{discr}"
+discr_root = Math.sqrt(discr)
+
 if discr > 0
-	x1 = (-b + Math.sqrt(discr)) / (2 * a)
-	x2 = (-b - Math.sqrt(discr)) / (2 * a)
-	puts "x1 = #{x1}, x2 = #{x2}"
+  x1 = (-b + discr_root) / (2 * a)
+  x2 = (-b - discr_root) / (2 * a)
+  puts "x1 = #{x1}, x2 = #{x2}"
 elsif discr == 0
-	x = -b/(2*a)
-	puts "x = #{x}"
+  x = -b/(2*a)
+  puts "x = #{x}"
 elsif discr < 0
   puts "No roots."
 end
-		
